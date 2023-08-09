0ce85e897c6d4be91ecdaf819a1634c92b44659d
bunnmei <kombinat.web@gmail.com>
2023-08-09 18:44:25 +0900

    Lチカテンプレ

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+    /*
+     * For examples (and inspiration), head to
+     *
+     *     https://github.com/Rahix/avr-hal/tree/main/examples
+     *
+     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
+     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
+     * examples available.
+     */
+
+    let mut led = pins.d5.into_output_high();
+    let mut stepper = pins.d7.into_output();
+
+    loop {
+        led.set_high();
+        arduino_hal::delay_ms(500);
+        led.set_low();
+        arduino_hal::delay_ms(500);
+    }
+    
+}
